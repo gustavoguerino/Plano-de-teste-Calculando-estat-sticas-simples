c69ca4f201401f95b37525b9251fc210e1d48a5e
Gustavo Guerino <gustavoguerino@hotmail.com>
2017-09-15 19:54:50 -0300

    Teste 3 - Verde

--- testeEstatisticas.py
+++ testeEstatisticas.py
@@ -1,14 +1,24 @@
-import unittest
+import unittest, io, sys, timeout_decorator
+
 from estatisticas import is_sizeSqn
 
 class TesteEstatisticas(unittest.TestCase):
+    @timeout_decorator.timeout(1)
     def test_seqnumeros_is_int(self):
         self.assertTrue(is_sizeSqn(1))
         
-    def teste_seqnumeros_not_int(self):
+    @timeout_decorator.timeout(1)
+    def test_seqnumeros_not_int(self):
         with self.assertRaises(TypeError):
             is_sizeSqn("testeDeSoftware") 
             is_sizeSqn("7.0") 
+
+    @timeout_decorator.timeout(1)
+    def test_valor_is_int(self):
+        data = "2"
+        with io.BytesIO(data) as sys.stdin:
+            self.assertTrue(is_sizeSqn(1))
+        sys.stdin = sys.__stdin__
            
 if __name__ == '__main__':
     unittest.main()
