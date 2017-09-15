4e16b2f319536abe90baa13c3aabc4c353b5e19a
Gustavo Guerino <gustavoguerino@hotmail.com>
2017-09-15 18:56:32 -0300

    Teste 2 - Vermelho

--- testeEstatisticas.py
+++ testeEstatisticas.py
@@ -5,5 +5,9 @@ class TesteEstatisticas(unittest.TestCase):
     def test_seqnumeros_is_int(self):
         self.assertTrue(is_sizeSqn(1))
         
+    def teste_seqnumeros_not_int(self):
+        with self.assertRaises(ValueError):
+            is_sizeSqn("testeDeSoftware") 
+           
 if __name__ == '__main__':
     unittest.main()
